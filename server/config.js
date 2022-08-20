c442e9a08e24175da8ed3cc11623cf7831906f73
waldron <waldron@mesegaming.co>
2022-08-20 15:57:53 +0800

    init

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  VAPIDKeys: {
+    publicKey: 'BISQsO7uTZvpsvX1f1Z-yY2EKXYdDfozjPi9lBviAoOTA9Gqa-5YRRPBgE9LcQE3nLi9X6TwDq6J68BTqFjhroA',
+    privateKey: 'kmYEcRr4FYz85t6Vq6Mkr8ZixMoblrM76zLCGJ8GGKs'
+  },
+  GCMAPIkey: 'BLD4haXBGkDPOcSEByvXDR8VxhyDRrtB7awQl6edq8rMq_egt2Z8jtNyf_2d4c4HU7gJVPVywHEB_Dr4NkcjdGo'
+}
